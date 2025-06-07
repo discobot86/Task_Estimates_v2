6a566c309b91f06e60c23109e9c0b64f6f71fab9
discobot86 <bourque.team@gmail.com>
2025-06-07 17:09:15 -0300

    Update connector.js

--- js/connector.js
+++ js/connector.js
@@ -1,12 +1,17 @@
+// js/connector.js
+
 window.TrelloPowerUp.initialize({
   'card-back-section': function(t, options){
+    // This tells Trello that your Power-Up supports dark mode
+    t.enable('theme');
+    
     return {
       title: 'Estimated Hours',
-      icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Ftime.svg?v=1621983611952', // A simple clock icon
+      icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Ftime.svg?v=1621983611952',
       content: {
         type: 'iframe',
         url: t.signUrl('./card-back-section.html'),
-        height: 50
+        height: 50 // You can adjust this height if needed
       }
     };
   }
