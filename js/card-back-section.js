039e114f6e0c4c648eff463200efa1e79db3df1e
discobot86 <bourque.team@gmail.com>
2025-06-07 17:01:08 -0300

    Create card-back-section.js

---
+++ js/card-back-section.js
@@ -0,0 +1,21 @@
+// This should be the ENTIRE content of js/card-back-section.js
+
+const t = window.TrelloPowerUp.iframe();
+
+// This tells Trello the Power-Up is theme-aware
+t.enable('theme');
+
+const estimatedHoursInput = document.getElementById('estimated-hours');
+
+t.render(function(){
+  return t.get('card', 'shared', 'estimatedHours')
+    .then(function(hours){
+      if(hours){
+        estimatedHoursInput.value = hours;
+      }
+    });
+});
+
+estimatedHoursInput.addEventListener('change', function(){
+  t.set('card', 'shared', 'estimatedHours', estimatedHoursInput.value);
+});
