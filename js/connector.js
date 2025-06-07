a70758e80630f49b4f612bba3ba3122f91a39a26
discobot86 <bourque.team@gmail.com>
2025-06-07 17:58:05 -0300

    Update connector.js

--- js/connector.js
+++ js/connector.js
@@ -1,16 +1,42 @@
 // js/connector.js
 
 window.TrelloPowerUp.initialize({
-  'card-back-section': function(t, options){
-    // This function should ONLY return the section's properties.
+  /* ---------------------- */
+  /* This is your existing card-back-section */
+  /* ---------------------- */
+  'card-back-section': function (t, options) {
     return {
       title: 'Estimated Hours',
-icon: 'https://discobot86.github.io/Task_estimates/img/icon.png',
+      icon: 'https://discobot86.github.io/Task_estimates/img/icon.png',
       content: {
         type: 'iframe',
         url: t.signUrl('./card-back-section.html'),
         height: 50
       }
     };
-  }
+  },
+
+  /* ---------------------- */
+  /* This is the new card-badges capability */
+  /* ---------------------- */
+  'card-badges': async function (t, options) {
+    // Get the stored hours from the card
+    const estimatedHours = await t.get('card', 'shared', 'estimatedHours');
+
+    // Check if we have a value and it's greater than 0
+    if (estimatedHours && parseFloat(estimatedHours) > 0) {
+      // If we do, return an array with our badge object
+      return [
+        {
+          text: estimatedHours + ' hrs',
+          icon: 'https://discobot86.github.io/Task_estimates/img/icon.png',
+          color: 'green',
+          refresh: 10
+        },
+      ];
+    } else {
+      // If there are no hours, return an empty array to show nothing
+      return [];
+    }
+  },
 });
